d780e3b7843705608f4159c053f149503118523a
Gustavo Rocha <sazaki.dc@hotmail.com>
2022-11-03 20:20:17 -0300

    mudança

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,8 +2,16 @@ public class MinhaClasse {
 
 
     public static void main(String[] args) {
-         System.out.print( "Olá turma, sejam bem-vindos");
 
+        String primeiroNome = "Gustavo";
+        String segundoNome = "Rocha";
+
+        String nomeCompleto = nomeCompleto(primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resulto do método: "  + primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
